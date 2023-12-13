dd2fd0d934a551c89bc73cd3570a3c5b688a4d99
Cosarca Maxim <cosarcamaxim@gmail.com>
2023-12-14 00:20:39 +0200

    Am inceput structurarea imaginilor

---
+++ addImage.js
@@ -0,0 +1,23 @@
+const bottomContainer = document.querySelector("#img-bottom-container")
+const topContainer = document.querySelector("#img-top-container")
+const cheeseContainer = document.querySelector("#img-cheese-container")
+const toppingsContainer = document.querySelector("#img-toppings-container")
+const meatContainer = document.querySelector("#img-meat-container")
+
+const meatOptions = document.querySelector("#select-meat option:checked");
+const bottomSauceOptions = document.querySelector("#select-bottom-sauce option:checked");
+const topSauceOptions = document.querySelector("#select-top-sauce option:checked");
+const cheeseOptions = document.querySelector("#select-chesse option:checked");
+const toppingsOptions = document.querySelector("#select-toppings option:checked");
+
+console.log(meatOptions.dataset.url);
+const addImage = (loc) => {
+
+
+}
+
+
+
+
+
+
